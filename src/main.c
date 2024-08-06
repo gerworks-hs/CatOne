70b661fcf1e689b7ebaac559b9bedc484899da53
itsgerliz <gerworks.hs@gmail.com>
2024-08-06 19:37:47 +0200

    Ft/ Mega commit

--- src/main.c
+++ src/main.c
@@ -3,19 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-int imethod; //Pointer to the input method flag, 1 for roll, 2 for index
+int rollCount; //Roll arguments count (number of arguments - first three mandatory arguments)
+int imethod; //Input method flag, 1 for roll, 2 for index
 char *archive_name; //Pointer to the string with the final archive name
 FILE **file_array; //Array of pointer to FILEs for the last arguments
 
-void exit_and_cleanup(const char *message, const int executionResult) { /////////////////////////
-	if (executionResult == 0) { //Print message when execution results in success
+void exit_and_cleanup(const char *message, const int executionResult) {
+	if (executionResult == 0) { //When execution results in success
+		if (imethod == 1) { //Roll
+			for (int i = 0; i <= rollCount; i++) {
+				fclose(file_array[i]);
+			}	
+		} else if (imethod == 2) { //Index
+			fclose(file_array[0]);
+		}
 		fprintf(stdout, "%s\n", message);
-	} else if (executionResult == 1) { //Print message when execution results in an error
+		free(file_array);
+		exit(EXIT_SUCCESS);
+	} else if (executionResult == 1) { //When execution results in failure
+		free(file_array);
 		fprintf(stderr, "%s\n", message);
+		exit(EXIT_FAILURE);
 	}
 }
 
-void parse_arguments(const int count, char **vector) {
+void parse_arguments(int count, char **vector) {
 	//Check enough arguments
 	if (count < 4) {
 		fprintf(stderr, "Incorrect app usage\nUsage: %s <input method> <archive name> <catone index file or files>\n", vector[0]);
@@ -32,26 +44,27 @@ void parse_arguments(const int count, char **vector) {
 	}
 	//Final archive name
 	archive_name = vector[2];
-	//Rest
-	if (imethod == 1) { //Roll
+	//Rest SINCE THIS POINT exit_and_cleanup() CAN BE CALLED
+	if (imethod == 1) { //Roll case
 		file_array = malloc(sizeof(FILE *) * (count - 3)); //Roll: define file_array as an arrray of pointers to FILEs long as argc - 3 initial args
-		for (int i = 3, int o = 0; i < count; i++, o++) { //i is set to 3 to start reading args avoiding the first three (0,1,2), o is set for the array
+		for (int i = 3, o = 0; i < count; i++, o++) { //i is set to 3 to start reading args avoiding the first three (0,1,2), o is set for the array
+			fprintf(stdout, "Trying to open file <%s>\n", vector[i]);
 			file_array[o] = fopen(vector[i], "rb"); //Define each member of the file array to a pointer to a FILE using the next arg and read binary mode
 			if (file_array[o] == NULL) { //Check for errors on fopen()
-				exit_and_cleanup(); ///////////////////////////////////////////////////////////
+				exit_and_cleanup("Cannot open current file", 1);
 			}
 		}		
-	} else if (imethod == 2) { //Index
+	} else if (imethod == 2) { //Index case
 		file_array = malloc(sizeof(FILE *)); //Index: define file_array as an array with a unique member pointing to the index file
 		file_array[0] = fopen(vector[3], "rb"); //Define the array unique member to a pointer to a FILE pointing to the index file
 		if (file_array[0] == NULL) { //Check for errors on fopen()
-			exit_and_cleanup(); /////////////////////////////////////////////////////////
+			exit_and_cleanup("Your index file cannot be opened", 1);
 		}
 	}
 }
 
 int main(int argc, char **argv) {
 	parse_arguments(argc, argv); //Check for correct app usage and initialize base variables
-	exit(EXIT_SUCCESS);
+	exit_and_cleanup("Reached end of normal execution", 0); //Normal end of execution
 }
 //Made by Gerworks-HS (@itsgerliz)
