e52e12aefcc9ce1b6d71f55897832aa963a53e49
itsgerliz <gerworks.hs@gmail.com>
2024-08-07 00:53:03 +0200

    Ft/ Fix/ Recoded the argument parsing, all good for now

--- src/main.c
+++ src/main.c
@@ -3,37 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-int rollCount; //Roll arguments count (number of arguments - first three mandatory arguments)
+int rollCount; //Roll arguments count (number of arguments - first three mandatory arguments), if index method is used then we will use the 4th
 int imethod; //Input method flag, 1 for roll, 2 for index
 char *archive_name; //Pointer to the string with the final archive name
 FILE **file_array; //Array of pointer to FILEs for the last arguments
 
-void exit_and_cleanup(const char *message, const int executionResult) {
-	if (executionResult == 0) { //When execution results in success
-		if (imethod == 1) { //Roll
-			for (int i = 0; i <= rollCount; i++) {
-				fclose(file_array[i]);
-			}	
-		} else if (imethod == 2) { //Index
-			fclose(file_array[0]);
-		}
-		fprintf(stdout, "%s\n", message);
-		free(file_array);
-		exit(EXIT_SUCCESS);
-	} else if (executionResult == 1) { //When execution results in failure
-		free(file_array);
-		fprintf(stderr, "%s\n", message);
-		exit(EXIT_FAILURE);
-	}
-}
-
 void parse_arguments(int count, char **vector) {
-	//Check enough arguments
-	if (count < 4) {
+	//CHECK FOR CORRECT USAGE
+	if (count < 4) { //Check if the user used at least 4 arguments
 		fprintf(stderr, "Incorrect app usage\nUsage: %s <input method> <archive name> <catone index file or files>\n", vector[0]);
 		exit(EXIT_FAILURE);
 	}
-	//Input method
+	//PARSE INPUT METHOD
 	if (strcmp("roll", vector[1]) == 0) { //Check input method
 		imethod = 1;
 	} else if (strcmp("index", vector[1]) == 0) { //Check input method
@@ -42,29 +23,50 @@ void parse_arguments(int count, char **vector) {
 		fprintf(stderr, "Not a valid input method\n");
 		exit(EXIT_FAILURE);
 	}
-	//Final archive name
-	archive_name = vector[2];
-	//Rest SINCE THIS POINT exit_and_cleanup() CAN BE CALLED
+	//PARSE FINAL ARCHIVE NAME
+	if (strlen(vector[2]) > 255) { //Check for too long final archive name
+		fprintf(stderr, "Final archive name is too long: <%s>\n", vector[2]);
+		exit(EXIT_FAILURE);
+	} else {
+		archive_name = vector[2]; //Equal global variable to argument
+	}
+	//REST OF THE ARGUMENTS 3 < count
 	if (imethod == 1) { //Roll case
 		file_array = malloc(sizeof(FILE *) * (count - 3)); //Roll: define file_array as an arrray of pointers to FILEs long as argc - 3 initial args
-		for (int i = 3, o = 0; i < count; i++, o++) { //i is set to 3 to start reading args avoiding the first three (0,1,2), o is set for the array
-			fprintf(stdout, "Trying to open file <%s>\n", vector[i]);
+		if (file_array == NULL) {
+			fprintf(stderr, "Fatal error trying to dinamically allocate memory, aborting...\n");
+			exit(EXIT_FAILURE);
+		}
+		for (int i = 3, o = 0; i < count; i++, o++) { //i = 3 to start reading args avoiding the first three (0,1,2)
 			file_array[o] = fopen(vector[i], "rb"); //Define each member of the file array to a pointer to a FILE using the next arg and read binary mode
 			if (file_array[o] == NULL) { //Check for errors on fopen()
-				exit_and_cleanup("Cannot open current file", 1);
+				fprintf(stderr, "Cannot open the file <%s>\n", vector[i]);
+				for (int i = 0; i < (count - 3); i++) {
+					fclose(file_array[i]); //Free each FILE pointer in file_array
+				}
+				free(file_array);
+				exit(EXIT_FAILURE);
 			}
+			fprintf(stdout, "File <%s> added for processing\n");
 		}		
 	} else if (imethod == 2) { //Index case
-		file_array = malloc(sizeof(FILE *)); //Index: define file_array as an array with a unique member pointing to the index file
-		file_array[0] = fopen(vector[3], "rb"); //Define the array unique member to a pointer to a FILE pointing to the index file
-		if (file_array[0] == NULL) { //Check for errors on fopen()
-			exit_and_cleanup("Your index file cannot be opened", 1);
+		file_array = malloc(sizeof(FILE *)); //Index: define file_array as a pointer to a pointer to a FILE (pointer to FILE *)
+		if (file_array == NULL) {
+			fprintf(stderr, "Fatal error trying to dinamically allocate memory, aborting...\n");
+			exit(EXIT_FAILURE);
+		}
+		*file_array = fopen(vector[3], "rb"); //Define the data pointed by file_array pointer as a pointer to a FILE (FILE *)
+		if (*file_array == NULL) { //Check for errors on fopen()
+			fprintf(stderr, "Cannot open the file <%s>\n", vector[3]);
+			fclose(*file_array);
+			free(file_array);
+			exit(EXIT_FAILURE);
 		}
 	}
 }
 
 int main(int argc, char **argv) {
-	parse_arguments(argc, argv); //Check for correct app usage and initialize base variables
-	exit_and_cleanup("Reached end of normal execution", 0); //Normal end of execution
+	parse_arguments(argc, argv);
+	exit(EXIT_SUCCESS);
 }
 //Made by Gerworks-HS (@itsgerliz)
